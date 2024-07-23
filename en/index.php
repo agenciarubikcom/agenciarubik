c68d5d50a59f29ce5f212401659af55a79ef4a99
agenciarubikcom <gsuarez@agenciarubik.com>
2024-07-23 11:17:02 -0300

    Nuevos Index

---
+++ en/index.php
@@ -0,0 +1,47 @@
+<?php
+$URLTitleES = "es/index";
+$URLTitleEN = "en/index";
+$Description = "Innovation and creativity in every click with Agencia Rubik. We transform your digital presence with impressive and personalized web designs.";
+include '../header.php';
+?>
+    <link rel="canonical" href="https://agenciarubik.com/en/index"/>
+<?php include '../headersecond.php';?>
+<style>
+    .site-menu-main, .agenciarubik-footer-top, .mobile-menu-trigger.light {
+        display: none;
+    }
+    .agenciarubik-section-padding {
+        padding: 125px 0 0px;
+    }
+    .agenciarubik-h1 {
+        text-indent: -99999px;
+        position: absolute;
+    }
+</style>
+
+<div class="section agenciarubik-section-padding">
+  <div class="container">
+    <div class="agenciarubik-section-title center">
+      <h1 class="agenciarubik-h1">Innovation and Creativity in every Click</h1>
+      <h2 class="rt-mb-20">Transforming Our Website for You!</h2>
+      <p>We are in the process of renewing our website. We want to give you a faster, more modern and friendly experience.<br><br>
+    During this time, we remain available for any questions or assistance you may need. Do not hesitate to contact us.</p>
+    </div>
+  </div>
+</div>
+
+
+<div class="section rt-mb-20">
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-12">
+        <?php include '../formcontact.php'; ?>
+      </div>
+    </div>
+  </div>
+</div>
+
+<!-- End section -->
+
+
+<?php include "../footer.php" ?>
---
+++ es/index.php
@@ -0,0 +1,47 @@
+<?php
+$URLTitleES = "es/index";
+$URLTitleEN = "en/index";
+$Description = "Innovación y creatividad en cada clic con Agencia Rubik. Transformamos tu presencia digital con diseños web impactantes y personalizados.";
+include '../header.php';
+?>
+    <link rel="canonical" href="https://agenciarubik.com/es/index"/>
+<?php include '../headersecond.php';?>
+<style>
+    .site-menu-main, .agenciarubik-footer-top, .mobile-menu-trigger.light {
+        display: none;
+    }
+    .agenciarubik-section-padding {
+        padding: 125px 0 0px;
+    }
+    .agenciarubik-h1 {
+        text-indent: -99999px;
+        position: absolute;
+    }
+</style>
+
+<div class="section agenciarubik-section-padding">
+  <div class="container">
+    <div class="agenciarubik-section-title center">
+      <h1 class="agenciarubik-h1">Innovación y Creatividad en cada Clic</h1>
+      <h2 class="rt-mb-20">¡Transformando Nuestra Web para Ti!</h2>
+      <p>Estamos en pleno proceso de renovación de nuestro sitio web. Queremos brindarte una experiencia más rápida, moderna y amigable.<br><br>
+    Durante este tiempo, seguimos disponibles para cualquier consulta o asistencia que necesites. No dudes en contactarnos.</p>
+    </div>
+  </div>
+</div>
+
+
+<div class="section rt-mb-20">
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-12">
+        <?php include '../formcontact.php'; ?>
+      </div>
+    </div>
+  </div>
+</div>
+
+<!-- End section -->
+
+
+<?php include "../footer.php" ?>

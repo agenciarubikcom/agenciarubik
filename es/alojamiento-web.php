c7df6ea5c7abd87ac79bd8bc0d3dd8a997666529
agenciarubikcom <gsuarez@agenciarubik.com>
2024-07-23 21:36:28 -0300

    Ordenar

---
+++ es/alojamiento-web.php
@@ -0,0 +1,180 @@
+<?php
+$URLTitleES = "es/alojamiento-web";
+$URLTitleEN = "en/web-hosting";
+$URLLink = "es/alojamiento-web";
+$Description = "Eleva Tu Presencia en Línea con Nuestro Servicio de SEO";
+include '../header.php';
+include 'nav.php';
+?>
+
+<div class="agenciarubik-breadcrumb">
+    <div class="container">
+        <h1 class="post__title">Alojamiento Web</h1>
+        <nav class="breadcrumbs">
+            <ul>
+                <li aria-current="page">Productos</li>
+            </ul>
+        </nav>
+
+    </div>
+</div>
+<!-- End breadcrumb -->
+
+<div class="section agenciarubik-section-padding">
+    <div class="container">
+        <div class="agenciarubik-section-title center">
+            <h2 class="rt-mb-20">Eleva tu presencia en Línea con nuestro servicio de SEO</h2>
+            <p>En el vasto mundo digital de hoy, tener un sitio web es solo el primer paso.</p>
+        </div>
+    </div>
+</div>
+<!-- End section -->
+
+<div class="section">
+    <div class="container">
+       <div class="pricing-btn">
+        <div class="toggle-btn">
+          <label>Mes</label>
+          <input class="form-check-input btn-toggle price-deck-trigger" type="checkbox" id="flexSwitchCheckDefault" data-pricing-trigger="" data-target="#table-price-value" checked="">
+          <label>Año</label>
+        </div>
+      </div>
+      <div class="row agenciarubik-pricing-four-column" id="table-price-value" data-pricing-dynamic="" data-value-active="monthly">
+        <div class="col-xl-3 col-md-6">
+          <div class="agenciarubik-pricing-wrap">
+            <div class="agenciarubik-pricing-header">
+              <h5>Free</h5>
+            </div>
+            <div class="agenciarubik-pricing-price">
+              <h2>$</h2>
+              <div class="agenciarubik-price dynamic-value" data-active="0" data-monthly="0" data-yearly="19"></div>
+              <p class="dynamic-value" data-active="/Monthly" data-monthly="/monthly" data-yearly="/Yearly"></p>
+            </div>
+            <div class="agenciarubik-pricing-description">
+              <p>This plan is typically limited in features and usage</p>
+            </div>
+            <div class="agenciarubik-pricing-body">
+              <p>Free plan includes:</p>
+              <ul>
+                <li><img src="assets/images/v3/check.png" alt="">Basic features</li>
+                <li><img src="assets/images/v3/check.png" alt="">Limited users &amp; usage</li>
+                <li><img src="assets/images/v3/check.png" alt="">No customer support</li>
+                <li><img src="assets/images/v3/check.png" alt="">30 day chat history</li>
+                <li><img src="assets/images/v3/check.png" alt="">10 Integrations</li>
+              </ul>
+            </div>
+            <a class="agenciarubik-pricing-btn" href="contact-us.html">Try it for free</a>
+          </div>
+        </div>
+        <div class="col-xl-3 col-md-6">
+          <div class="agenciarubik-pricing-wrap">
+            <div class="agenciarubik-pricing-header">
+              <h5>Basic</h5>
+            </div>
+            <div class="agenciarubik-pricing-price">
+              <h2>$</h2>
+              <div class="agenciarubik-price dynamic-value" data-active="19" data-monthly="19" data-yearly="39"></div>
+              <p class="dynamic-value" data-active="/Monthly" data-monthly="/monthly" data-yearly="/Yearly"></p>
+            </div>
+            <div class="agenciarubik-pricing-description">
+              <p>A simple and affordable plan only for small businesses</p>
+            </div>
+            <div class="agenciarubik-pricing-body">
+              <p>Basic plan includes:</p>
+              <ul>
+                <li><img src="assets/images/v3/check.png" alt="">Core features</li>
+                <li><img src="assets/images/v3/check.png" alt="">Increased limits</li>
+                <li><img src="assets/images/v3/check.png" alt="">Priority support</li>
+                <li><img src="assets/images/v3/check.png" alt="">Unlimited chat history</li>
+                <li><img src="assets/images/v3/check.png" alt="">20 Integrations</li>
+              </ul>
+            </div>
+            <a class="agenciarubik-pricing-btn" href="contact-us.html">Try it for free</a>
+          </div>
+        </div>
+        <div class="col-xl-3 col-md-6">
+          <div class="agenciarubik-pricing-wrap active">
+            <div class="agenciarubik-pricing-header">
+              <h5>Standard</h5>
+            </div>
+            <div class="agenciarubik-pricing-price">
+              <h2>$</h2>
+              <div class="agenciarubik-price dynamic-value" data-active="29" data-monthly="29" data-yearly="59"></div>
+              <p class="dynamic-value" data-active="/Monthly" data-monthly="/monthly" data-yearly="/Yearly"></p>
+            </div>
+            <div class="agenciarubik-pricing-description">
+              <p>This plan is geared toward growing businesses</p>
+            </div>
+            <div class="agenciarubik-pricing-body">
+              <p>Standard plan includes:</p>
+              <ul>
+                <li><img src="assets/images/v3/check.png" alt="">Expanded features</li>
+                <li><img src="assets/images/v3/check.png" alt="">Increased users</li>
+                <li><img src="assets/images/v3/check.png" alt="">Priority email support</li>
+                <li><img src="assets/images/v3/check.png" alt="">Unlimited chat history</li>
+                <li><img src="assets/images/v3/check.png" alt="">30 Integrations</li>
+              </ul>
+            </div>
+            <a class="agenciarubik-pricing-btn active" href="contact-us.html">Try it for free</a>
+          </div>
+        </div>
+        <div class="col-xl-3 col-md-6">
+          <div class="agenciarubik-pricing-wrap">
+            <div class="agenciarubik-pricing-header">
+              <h5>Enterprise</h5>
+            </div>
+            <div class="agenciarubik-pricing-price">
+              <h2>$</h2>
+              <div class="agenciarubik-price dynamic-value" data-active="59" data-monthly="59" data-yearly="99"></div>
+              <p class="dynamic-value" data-active="/Monthly" data-monthly="/monthly" data-yearly="/Yearly"></p>
+            </div>
+            <div class="agenciarubik-pricing-description">
+              <p>Tailored to the unique needs of large enterprises</p>
+            </div>
+            <div class="agenciarubik-pricing-body">
+              <p>Enterprise plan includes:</p>
+              <ul>
+                <li><img src="assets/images/v3/check.png" alt="">Customized features</li>
+                <li><img src="assets/images/v3/check.png" alt="">Scalability &amp; security </li>
+                <li><img src="assets/images/v3/check.png" alt="">Account manager</li>
+                <li><img src="assets/images/v3/check.png" alt="">Unlimited chat history</li>
+                <li><img src="assets/images/v3/check.png" alt="">50 Integrations</li>
+              </ul>
+            </div>
+            <a class="agenciarubik-pricing-btn" href="contact-us.html">Try it for free</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+<div class="container mb-5">
+    <div class="row">
+        <div class="col-lg-5">
+            <div class="agenciarubik-v3-thumb">
+                <img src="https://agenciarubik.com/assets/images/seo.svg" title="SEO" alt="SEO">
+
+            </div>
+        </div>
+        <div class="col-lg-7">
+            <div class="agenciarubik-default-content m-left">
+                <h2>Transforma tu sitio web en un imán de tráfico</h2>
+                <p>Con nuestro servicio de SEO para páginas web, te ayudamos a superar a la competencia, aumentar tu visibilidad en línea y atraer a más clientes potenciales. No importa el tamaño de tu negocio o la industria en la que operes, una estrategia de SEO efectiva es fundamental para tu éxito en línea.<br><br><b>Contáctanos hoy y comienza el viaje para maximizar tu presencia en línea y llevar tu negocio al próximo nivel.</p>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="section my-5">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12">
+                <?php include '../formcontact.php'; ?>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- End section -->
+
+
+<?php include "../footer.php" ?>

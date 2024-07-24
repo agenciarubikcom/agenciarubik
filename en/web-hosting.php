202b8ca3f487229cbbbe46211140e76ffa234578
agenciarubikcom <109293259+agenciarubikcom@users.noreply.github.com>
2024-07-24 17:51:27 -0300

    Web Hosting

---
+++ en/web-hosting.php
@@ -0,0 +1,227 @@
+<?php
+$URLTitleES = "es/alojamiento-web";
+$URLTitleEN = "en/web-hosting";
+$URLLink = "en/web-hosting";
+$Description = "Ofrecemos planes flexibles, seguridad robusta y soporte técnico 24/7. ¡No te quedes con dudas! Contáctanos y elige el mejor plan para tu sitio.";
+include '../header.php';
+include 'nav.php';
+?>
+
+<div class="agenciarubik-breadcrumb">
+    <div class="container">
+        <h1 class="post__title">Web Hosting</h1>
+        <nav class="breadcrumbs">
+            <ul>
+                <li aria-current="page">Products</li>
+            </ul>
+        </nav>
+
+    </div>
+</div>
+<!-- End breadcrumb -->
+
+<div class="section mt-5">
+    <div class="container">
+        <div class="agenciarubik-section-title center">
+            <h2 class="rt-mb-20">Your trusted web hosting solution</h2>
+            <p>We understand that your online presence is crucial to the success of your business. That's why we've created a high-quality web hosting service designed to give you the best experience possible.</p>
+        </div>
+    </div>
+</div>
+<!-- End section -->
+
+<div class="section mb-5">
+    <div class="container">
+      <div class="row agenciarubik-pricing-four-column" id="table-price-value">
+        <div class="col-xl-4 col-md-6">
+          <div class="agenciarubik-pricing-wrap">
+            <div class="agenciarubik-pricing-header">
+              <h5>Lennon</h5>
+            </div>
+            <div class="agenciarubik-pricing-price">
+              <h2>$</h2>
+              <div class="agenciarubik-price dynamic-value" data-active="60"></div>
+              <p class="dynamic-value" data-active="by year"></p>
+            </div>
+            <div class="agenciarubik-pricing-description">
+              <p>Ideal for those starting their web journey with creativity and vision.<br><br></p>
+            </div>
+            <div class="agenciarubik-pricing-body">
+              <ul>
+                <li><img src="../assets/images/v3/check.png" alt="">Up to 3 Domains</li>
+                <li><img src="../assets/images/v3/check.png" alt="">20 GB SSD</li>
+                <li><img src="../assets/images/v3/check.png" alt="">Up to 30 Emails</li>
+                <li><img src="../assets/images/v3/check.png" alt="">Support via email 24/7</li>
+                <li><br></li>
+                <li><br></li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div class="col-xl-4 col-md-6">
+          <div class="agenciarubik-pricing-wrap active">
+            <div class="agenciarubik-pricing-header">
+              <h5>Jagger</h5>
+            </div>
+            <div class="agenciarubik-pricing-price">
+              <h2>$</h2>
+              <div class="agenciarubik-price dynamic-value" data-active="95"></div>
+              <p class="dynamic-value" data-active="by year"></p>
+            </div>
+            <div class="agenciarubik-pricing-description">
+              <p>Perfect for expanding businesses that need reliable and efficient service.<br><br></p>
+            </div>
+            <div class="agenciarubik-pricing-body">
+
+              <ul>
+                <li><img src="../assets/images/v3/check.png" alt="">Unlimited Domains</li>
+                <li><img src="../assets/images/v3/check.png" alt="">Unlimited SSD Space</li>
+                <li><img src="../assets/images/v3/check.png" alt="">Unlimited Emails</li>
+                <li><img src="../assets/images/v3/check.png" alt="">Dedicated IP</li>
+                <li><img src="../assets/images/v3/check.png" alt="">AutoBackup</li>
+                <li><img src="../assets/images/v3/check.png" alt="">Support via email 24/7</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div class="col-xl-4 col-md-6">
+          <div class="agenciarubik-pricing-wrap">
+            <div class="agenciarubik-pricing-header">
+              <h5>McCartney</h5>
+            </div>
+            <div class="agenciarubik-pricing-price">
+              <h2>$</h2>
+              <div class="agenciarubik-price dynamic-value" data-active="150"></div>
+              <p class="dynamic-value" data-active="by year"></p>
+            </div>
+            <div class="agenciarubik-pricing-description">
+              <p>Designed for those looking for maximum reliability and professional performance in their online presence.</p>
+            </div>
+            <div class="agenciarubik-pricing-body">
+              <ul>
+                <li><img src="../assets/images/v3/check.png" alt="">Unlimited Domains</li>
+                <li><img src="../assets/images/v3/check.png" alt="">50 GB SSD</li>
+                <li><img src="../assets/images/v3/check.png" alt="">Unlimited Emails</li>
+                <li><img src="../assets/images/v3/check.png" alt="">Dedicated IP</li>
+                <li><img src="../assets/images/v3/check.png" alt="">AutoBackup</li>
+                <li><img src="../assets/images/v3/check.png" alt="">Support via email 24/7</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="section">
+    <div class="container">
+      <div class="agenciarubik-section-title agenciarubik-two-column-title">
+        <div class="row">
+          <div class="col-lg-7">
+            <h2>Do not entertain doubts!</h2>
+          </div>
+          <div class="col-lg-5 d-flex align-items-center">
+            <p>We know that making a decision about web hosting is important and we want you to feel completely confident. That's why our team is always available to answer any questions you may have. Do not hesitate to contact us before hiring. We want to make sure you understand all the features and benefits of our plans so you can make the best decision for your business.</p>
+          </div>
+        </div>
+      </div>
+
+      <div class="agenciarubik-table-wrap">
+        <table>
+          <thead>
+            <tr>
+              <th>Characteristic</th>
+              <th>Lennon</th>
+              <th>Jagger</th>
+              <th>McCartney</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Disc space</td>
+              <td>20 GB SSD</td>
+              <td>Unlimited SSD</td>
+              <td>50 GB SSD</td>
+            </tr>
+            <tr>
+              <td>File and folder limit</td>
+              <td>300.000</td>
+              <td>300.000</td>
+              <td>600.000</td>
+            </tr>
+            <tr>
+              <td>Bandwidth</td>
+              <td>Unlimited</td>
+              <td>Unlimited</td>
+              <td>Unlimited</td>
+            </tr>
+            <tr>
+              <td>Domains</td>
+              <td>3</td>
+              <td>Unlimited</td>
+              <td>Unlimited</td>
+            </tr>
+            <tr>
+              <td>Subdomains</td>
+              <td>30</td>
+              <td>Unlimited</td>
+              <td>Unlimited</td>
+            </tr>
+            <tr>
+              <td>Backups</td>
+              <td>Twice a week</td>
+              <td>Twice a week<br>Autobackup</td>
+              <td>Twice a week<br>Autobackup</td>
+            </tr>
+            <tr>
+              <td>Dedicated IP</td>
+              <td><img src="../assets/images/icon/minus.svg" alt=""></td>
+              <td><img src="../assets/images/icon/check.svg" alt=""></td>
+              <td><img src="../assets/images/icon/check.svg" alt=""></td>
+            </tr>
+            <tr>
+              <td>Email accounts</td>
+              <td>30</td>
+              <td>Unlimited</td>
+              <td>Unlimited</td>
+            </tr>
+            <tr>
+              <td>MySQL Bases</td>
+              <td>50</td>
+              <td>Unlimited</td>
+              <td>Unlimited</td>
+            </tr>
+            <tr>
+              <td>Support via email 24/7</td>
+              <td><img src="../assets/images/icon/check.svg" alt=""></td>
+              <td><img src="../assets/images/icon/check.svg" alt=""></td>
+              <td><img src="../assets/images/icon/check.svg" alt=""></td>
+            </tr>
+
+          </tbody>
+        </table>
+      </div>
+    </div>
+  </div>
+
+  <div class="section mt-5">
+    <div class="container">
+        <div class="agenciarubik-section-title center">
+            <p>If you need additional information or more detailed specifications, please do not hesitate to contact us.</p>
+        </div>
+    </div>
+</div>
+
+<div class="section my-5">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12">
+                <?php include '../formcontact.php'; ?>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- End section -->
+
+
+<?php include "../footer.php" ?>

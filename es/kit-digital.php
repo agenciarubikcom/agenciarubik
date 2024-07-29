2008e646b83e63d61da1e7e5ff7acab1bc4b53ad
agenciarubikcom <gsuarez@agenciarubik.com>
2024-07-29 15:53:55 -0300

    Kit Digital

---
+++ es/kit-digital.php
@@ -0,0 +1,92 @@
+<?php
+$URLTitleES = "es/kit-digital";
+$URLTitleEN = "en/digital-kit";
+$URLLink = "es/kit-digital";
+$Description = "Descubre el Kit Digital: Todo lo que necesitas para establecer una presencia en línea profesional.";
+include '../header.php';
+include 'nav.php';
+?>
+
+
+<div class="agenciarubik-breadcrumb">
+    <div class="container">
+        <h1 class="post__title">Kit Digital</h1>
+        <nav class="breadcrumbs">
+            <ul>
+                <li aria-current="page">Productos</li>
+            </ul>
+        </nav>
+    </div>
+</div>
+
+<div class="section agenciarubik-section-padding2">
+    <div class="container">
+        <div class="row align-items-center">
+            <div class="col-lg-6">
+                <div class="agenciarubik-default-content">
+                    <h2 class="rt-mb-20">Tu puerta de entrada al mundo online</h2>
+                </div>
+            </div>
+            <div class="col-lg-6">
+                <div class="agenciarubik-v3-thumb p-5">
+                    <p>¿Eres una persona emprendedora o una pequeña empresa sin presencia en internet? ¿Estás buscando una manera fácil y efectiva de llevar tu negocio al siguiente nivel? ¡Tenemos la solución perfecta para ti!</p>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</div>
+
+<div class="container pt-5">
+    <div class="agenciarubik-section-title ">
+        <h3>¿Qué es el Kit Digital?</h3>
+        <p class="mt-5">Un paquete integral diseñado especialmente para aquellos que desean dar el salto al mundo digital sin complicaciones. Nuestro <b>Kit Digital</b> incluye todo lo que necesitas para establecer una presencia en línea profesional y atractiva, ¡y mucho más!<br><br>
+
+            El Kit Digital incluye un <b>sitio web personalizado</b> con un diseño profesional y moderno que reflejará la esencia de tu negocio. Además, optimizamos tu sitio web con técnicas SEO para mejorar tu visibilidad en Google y otros buscadores. Aseguramos que tu sitio sea responsivo, lo que significa que se verá increíble en cualquier dispositivo, ya sea un móvil, tablet o computadora. También incluimos un formulario de contacto para facilitar la comunicación con tus clientes potenciales.<br><br>
+
+            El servicio de <b>alojamiento web</b> que ofrecemos es de alta calidad, garantizando que tu sitio web esté siempre en línea de manera segura y confiable. Incluimos un certificado SSL para proteger la información de tus visitantes y ganar su confianza.<br><br>
+
+            Te asistimos en la elección y registro de un <b>dominio personalizado</b>, asegurándonos de que el nombre de dominio sea perfecto para tu negocio.<br><br>
+
+            Además, proporcionamos un <b>pack de imágenes profesionales</b> diseñadas específicamente para tus redes sociales, facilitando la creación de publicaciones atractivas para Facebook, Instagram, X y LinkedIn. También incluimos plantillas personalizables para que puedas mantener una estética coherente en tus publicaciones.<br><br>
+
+            Nuestro Kit Digital también incluye mantenimiento y soporte técnico 24/7 vía mail para resolver cualquier problema y realizar actualizaciones de seguridad constantes. Configuramos Google Analytics para que obtengas datos valiosos sobre el tráfico y rendimiento de tu sitio. También incluimos <b>SEO local</b> para ayudarte a atraer a clientes en tu área, mejorando tu presencia en búsquedas locales y mapas.
+        </p>
+    </div>
+
+</div>
+
+<div class="section">
+    <div class="container">
+        <div class="agenciarubik-section-title agenciarubik-two-column-title">
+            <div class="row">
+                <div class="col-lg-6">
+                    <div class="agenciarubik-v3-thumb p-5">
+                        <img src="https://agenciarubik.com/assets/images/kit-digital.svg" title="Kit Digital" alt="Kit Digital">
+                    </div>
+                </div>
+                <div class="col-lg-6">
+                    <h2>¡Empieza Hoy!</h2>
+                    <p>Establecer tu presencia en línea nunca ha sido tan fácil. Contáctanos y descubre cómo el Kit Digital puede ayudarte a crecer y destacar en el mundo digital. ¡Da el primer paso hacia el éxito con el Kit Digital!</p>
+                </div>
+            </div>
+        </div>
+
+    </div>
+</div>
+
+</div>
+
+<div class="section my-5">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12">
+                <?php include '../formcontact.php'; ?>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- End section -->
+
+
+<?php include "../footer.php" ?>

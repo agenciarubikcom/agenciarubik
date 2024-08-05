9d5f7d1d59439757d591c124dee78fe2e0e14b48
agenciarubikcom <gsuarez@agenciarubik.com>
2024-08-05 20:44:09 -0300

    Dominios

---
+++ en/domains.php
@@ -0,0 +1,148 @@
+<?php
+$URLTitleES = "es/dominios";
+$URLTitleEN = "en/domains";
+$URLLink = "en/domains";
+$Description = "Domain registration in the name of the client or agency, and renewal management. Discover available options and make sure you get the best name for your online presence";
+include '../header.php';
+include 'nav.php';
+?>
+
+<div class="agenciarubik-breadcrumb">
+  <div class="container">
+    <h1 class="post__title">Domains</h1>
+    <nav class="breadcrumbs">
+      <ul>
+        <li aria-current="page">Products</li>
+      </ul>
+    </nav>
+
+  </div>
+</div>
+<!-- End breadcrumb -->
+
+
+
+<div class="section agenciarubik-section mt-5">
+
+  <div class="container mb-5 ">
+    <div class="row d-flex align-items-center">
+      <div class="col-lg-5">
+        <div class="agenciarubik-v3-thumb">
+          <img src="https://agenciarubik.com/assets/images/seo.svg" title="SEO" alt="SEO">
+
+        </div>
+      </div>
+      <div class="col-lg-7">
+        <div class="agenciarubik-default-content">
+          <h2 class="rt-mb-20">Find the perfect name</h2>
+          <p>We understand that a good domain is essential for the success of your internet presence. We offer a complete domain sales service, designed to help you find and register the perfect name for your business, project or personal brand.</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+<!-- End section -->
+
+<div class="section agenciarubik-section">
+  <div class="container">
+    <div class="col-lg-12">
+      <div class="agenciarubik-default-content">
+        <h2 class="mb-5">Our services include</h2>
+      </div>
+    </div>
+    <div class="agenciarubik-team-skill-wrap">
+      <div class="agenciarubik-team-skill">
+        <h3>Domain Registration</h3>
+        <p>We help you register your domain in the most popular extensions (.com, .net, .org, among others). We make sure that the domain is registered in the name of the client or, if you prefer, in the name of our agency to facilitate management.</p>
+      </div>
+      <div class="agenciarubik-team-skill">
+        <h3>Naming Job</h3>
+        <p>We know that choosing the right name can be a challenge. Our team of experts performs exhaustive naming work to help you find a domain that is memorable, relevant and aligned with your brand.</p>
+      </div>
+      <div class="agenciarubik-team-skill">
+        <h3>Availability Check</h3>
+        <p>We verify the availability of domains in real time and inform you about the available options. Please note that costs and availability may vary depending on demand and domain size.</p>
+      </div>
+
+      <div class="agenciarubik-team-skill">
+        <h3>Renewal Management</h3>
+        <p>We take care of your domain renewal so you don't have to worry about losing your name online. We notify you in advance of renewal dates and manage the entire process for you.</p>
+      </div>
+
+      <div class="agenciarubik-team-skill">
+        <h3>Domain Transfers</h3>
+        <p>We facilitate the transfer of domains between registrars, ensuring a quick and hassle-free process.</p>
+      </div>
+
+      <div class="agenciarubik-team-skill">
+        <h3>Privacy Protection</h3>
+        <p>We offer privacy protection services so that your personal data is not visible in public domain registries, thus guaranteeing your security and peace of mind.</p>
+      </div>
+
+    </div>
+  </div>
+</div>
+
+<div class="container mb-5">
+  <div class="row d-flex align-items-center">
+
+    <div class="col-lg-7">
+
+      <div class="agenciarubik-default-content mt-5">
+        <h2 class="mb-3">Why choose us</h2>
+      </div>
+
+      <div class="agenciarubik-default-content left">
+
+        <div>
+          <h3>Personalized Advice</h3>
+          <p>We work with you to understand your needs and objectives, providing personalized recommendations to choose the best domain for your project.</p>
+
+          <h3>Transparency and Flexibility</h3>
+          <p>We offer clear options for domain registration, either in your name or in that of our agency, depending on what suits you best.</p>
+
+          <h3>Experience and Professionalism</h3>
+          <p>With years of experience in the sector, our team is committed to offering high quality service and continuous support.</p>
+
+
+        </div>
+      </div>
+    </div>
+    <div class="col-lg-5">
+      <div class="agenciarubik-v3-thumb">
+        <img src="https://agenciarubik.com/assets/images/seo.svg" title="SEO" alt="SEO">
+
+      </div>
+    </div>
+  </div>
+</div>
+
+<div class="section agenciarubik-section-padding mt-5" style="background-color:#ABFB4F;">
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-6">
+        <div class="agenciarubik-default-content cta dark">
+          <h2>
+            Start today</h2>
+        </div>
+      </div>
+      <div class="col-lg-6 d-flex align-items-center dark">
+      <p>Don't let choosing the right domain get in the way of your online success. Contact us today for a free consultation and find out how we can help you find and register the perfect name for your online presence.</p>
+      </div>
+    </div>
+  </div>
+</div>
+
+<div class="section my-5">
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-12">
+        <?php include '../formcontact.php'; ?>
+      </div>
+    </div>
+  </div>
+</div>
+<!-- End section -->
+
+
+<?php include "../footer.php" ?>
---
+++ es/dominios.php
@@ -0,0 +1,144 @@
+<?php
+$URLTitleES = "es/dominios";
+$URLTitleEN = "en/domains";
+$URLLink = "es/dominios";
+$Description = "Registro de dominios a nombre del cliente o de la agencia, y gestión de renovaciones. Descubre opciones disponibles y asegúrate de obtener el mejor nombre para tu presencia en línea";
+include '../header.php';
+include 'nav.php';
+?>
+
+<div class="agenciarubik-breadcrumb">
+    <div class="container">
+        <h1 class="post__title">Dominios</h1>
+        <nav class="breadcrumbs">
+            <ul>
+                <li aria-current="page">Productos</li>
+            </ul>
+        </nav>
+
+    </div>
+</div>
+<!-- End breadcrumb -->
+
+
+
+<div class="section agenciarubik-section mt-5">
+
+    <div class="container mb-5 ">
+        <div class="row d-flex align-items-center">
+            <div class="col-lg-5">
+                <div class="agenciarubik-v3-thumb">
+                    <img src="https://agenciarubik.com/assets/images/seo.svg" title="SEO" alt="SEO">
+
+                </div>
+            </div>
+            <div class="col-lg-7">
+                <div class="agenciarubik-default-content">
+                    <h2 class="rt-mb-20">Encuentra el nombre perfecto</h2>
+                    <p>Entendemos que un buen dominio es esencial para el éxito de tu presencia en internet. Ofrecemos un servicio completo de venta de dominios, diseñado para ayudarte a encontrar y registrar el nombre perfecto para tu negocio, proyecto o marca personal.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- End section -->
+
+<div class="section agenciarubik-section">
+    <div class="container">
+        <div class="col-lg-12">
+            <div class="agenciarubik-default-content">
+                <h2 class="mb-5">Nuestros servicios incluyen</h2>
+            </div>
+        </div>
+        <div class="agenciarubik-team-skill-wrap">
+            <div class="agenciarubik-team-skill">
+                <h3>Registro de Dominios</h3>
+                <p>Te ayudamos a registrar tu dominio en las extensiones más populares (.com, .net, .org, entre otras). Nos aseguramos de que el dominio quede registrado a nombre del cliente o, si lo prefieres, a nombre de nuestra agencia para facilitar la gestión.</p>
+            </div>
+            <div class="agenciarubik-team-skill">
+                <h3>Trabajo de Naming</h3>
+                <p>Sabemos que elegir el nombre adecuado puede ser un desafío. Nuestro equipo de expertos realiza un trabajo de naming exhaustivo para ayudarte a encontrar un dominio que sea memorable, relevante y alineado con tu marca.</p>
+            </div>
+            <div class="agenciarubik-team-skill">
+                <h3>Verificación de Disponibilidad</h3>
+                <p>Verificamos la disponibilidad de los dominios en tiempo real y te informamos sobre las opciones disponibles. Ten en cuenta que los costos y la disponibilidad pueden variar según la demanda y la extensión del dominio.</p>
+            </div>
+
+            <div class="agenciarubik-team-skill">
+                <h3>Gestión de Renovaciones</h3>
+                <p>Nos encargamos de la renovación de tu dominio para que no tengas que preocuparte por perder tu nombre en línea. Te notificamos con antelación sobre las fechas de renovación y gestionamos todo el proceso por ti.</p>
+            </div>
+
+            <div class="agenciarubik-team-skill">
+                <h3>Transferencias de Dominio</h3>
+                <p>Facilitamos la transferencia de dominios entre registradores, asegurando un proceso rápido y sin complicaciones.</p>
+            </div>
+
+            <div class="agenciarubik-team-skill">
+                <h3>Protección de Privacidad</h3>
+                <p>Ofrecemos servicios de protección de privacidad para que tus datos personales no sean visibles en los registros públicos de dominios, garantizando así tu seguridad y tranquilidad.</p>
+            </div>
+
+        </div>
+    </div>
+</div>
+
+<div class="container mb-5">
+    <div class="row d-flex align-items-center">
+
+        <div class="col-lg-7">
+
+            <div class="agenciarubik-default-content mt-5">
+                <h2 class="mb-3">Por qué elegirnos</h2>
+            </div>
+
+            <div class="agenciarubik-default-content left">
+
+                <div>
+                <h3>Asesoramiento Personalizado</h3><p>Trabajamos contigo para entender tus necesidades y objetivos, proporcionando recomendaciones personalizadas para elegir el mejor dominio para tu proyecto.</p>
+
+                <h3>Transparencia y Flexibilidad</h3><p>Ofrecemos opciones claras para el registro de dominios, ya sea a tu nombre o al de nuestra agencia, según lo que más te convenga.</p>
+
+                <h3>Experiencia y Profesionalismo</h3><p>Con años de experiencia en el sector, nuestro equipo está comprometido en ofrecer un servicio de alta calidad y soporte continuo.</p>
+
+
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-5">
+            <div class="agenciarubik-v3-thumb">
+                <img src="https://agenciarubik.com/assets/images/seo.svg" title="SEO" alt="SEO">
+
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="section agenciarubik-section-padding mt-5" style="background-color:#ABFB4F;">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-6">
+          <div class="agenciarubik-default-content cta dark">
+            <h2>Comienza hoy mismo</h2>
+          </div>
+        </div>
+        <div class="col-lg-6 d-flex align-items-center dark">
+          <p>No dejes que la elección de un dominio adecuado sea un obstáculo para tu éxito en línea. Contáctanos hoy para una consulta gratuita y descubre cómo podemos ayudarte a encontrar y registrar el nombre perfecto para tu presencia en internet.</p>
+        </div>
+      </div>
+    </div>
+  </div>
+
+<div class="section my-5">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12">
+                <?php include '../formcontact.php'; ?>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- End section -->
+
+
+<?php include "../footer.php" ?>

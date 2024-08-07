795139b8586886325a446e0e4fab1a973aa8532b
agenciarubikcom <109293259+agenciarubikcom@users.noreply.github.com>
2024-08-07 14:09:42 -0300

    Cambio color

--- en/email-marketing.php
+++ en/email-marketing.php
@@ -22,8 +22,8 @@ include 'en/nav.php';
 <div class="container">
   <div class="row align-items-center">
   <div class="col-lg-6">
-      <div class="agenciarubik-default-content m-left">
-      <h2 class="rt-mb-20">Maximize Your Impact with Email Marketing</h2>
+      <div class="agenciarubik-default-content">
+      <h2 class="rt-mb-20">Maximize your impact with email marketing</h2>
       <p>In the digital age, email remains one of the most direct and personal ways to communicate with your customers. Our Email Marketing service is designed to help you build lasting relationships with your audience and convert those links into sales. Here we explain how we make your success a reality.</p>
       </div>
     </div>
@@ -40,30 +40,30 @@ include 'en/nav.php';
   <div class="container">
     <div class="agenciarubik-team-skill-wrap">
       <div class="agenciarubik-team-skill">
-        <h3>Smart Segmentation</h3>
+        <h3>Smart segmentation</h3>
         <p>We start by segmenting your audience to personalize messages so they resonate with each group's specific needs and preferences. We use demographic data, purchasing behavior and engagement levels to ensure the right message reaches the right person at the right time.</p>
       </div>
       <div class="agenciarubik-team-skill">
-        <h3>Creating Attractive Content</h3>
+        <h3>Creating attractive content</h3>
         <p>Our content and graphic design experts create visually engaging email campaigns and write persuasive copy that captures the essence of your brand. From newsletters to exclusive promotions, each email is carefully designed to engage, educate and convert.</p>
       </div>
       <div class="agenciarubik-team-skill">
-        <h3>Automation and Personalization</h3>
+        <h3>Automation and personalization</h3>
         <p>We implement automation workflows that send emails based on specific actions of your subscribers, such as welcoming new users, abandoned cart reminders, and post-purchase tracking. This ensures timely and relevant communication that increases customer loyalty and lifetime value.</p>
       </div>
 
       <div class="agenciarubik-team-skill">
-        <h3>A/B Testing and Database Optimization</h3>
+        <h3>A/B testing and database optimization</h3>
         <p>We regularly A/B test key elements of your email campaigns, such as subject lines, images, and calls to action, to identify what resonates best with your audience. This information optimizes future campaigns, improving open rates, clicks and conversions. We also maintain and update your contact lists to ensure maximum effectiveness.</p>
       </div>
 
       <div class="agenciarubik-team-skill">
-        <h3>Deep Analysis and Reporting</h3>
+        <h3>Deep Analysis and reporting</h3>
         <p>We provide you with detailed analyzes and understandable reports that show the performance of your email marketing campaigns. From open rates to return on investment (ROI), we meticulously monitor key performance indicators to help you make data-driven decisions.</p>
       </div>
 
       <div class="agenciarubik-team-skill">
-        <h3>Compliance and Best Practices</h3>
+        <h3>Compliance and best practices</h3>
         <p>We ensure that all campaigns comply with privacy laws and email marketing regulations, including GDPR and CAN-SPAM, protecting your reputation and ensuring the trust of your subscribers.</p>
       </div>
 
@@ -73,7 +73,7 @@ include 'en/nav.php';
 
 <div class="container pt-5">
       <div class="agenciarubik-section-title center">
-        <h2>Transform Your Email Marketing Strategy Today</h2>
+        <h2>Transform your email marketing strategy today</h2>
         <p class="mt-5">With our Email Marketing service, you have the guarantee of not only reaching your audience, but also having a significant impact, promoting conversion and loyalty. Whether you're launching a new product, building customer loyalty, or looking to increase your sales, we're here to help you achieve your marketing goals with strategies that deliver results.<br><br>Contact us to start creating campaigns that leave their mark and lead to the success of your business.
         </p>
       </div>
--- es/email-marketing.php
+++ es/email-marketing.php
@@ -23,8 +23,8 @@ include 'nav.php';
   <div class="container">
     <div class="row align-items-center">
       <div class="col-lg-6">
-        <div class="agenciarubik-default-content m-left">
-          <h2 class="rt-mb-20">Maximiza Tu Impacto con Email Marketing</h2>
+        <div class="agenciarubik-default-content">
+          <h2 class="rt-mb-20">Maximiza tu impacto con email marketing</h2>
           <p>En la era digital, el correo electrónico sigue siendo una de las formas más directas y personales de comunicación con tus clientes. Nuestro servicio de Email Marketing está diseñado para ayudarte a construir relaciones duraderas con tu audiencia y convertir esos vínculos en ventas. Aquí te explicamos cómo hacemos realidad tu éxito.</p>
         </div>
       </div>
@@ -41,30 +41,30 @@ include 'nav.php';
   <div class="container">
     <div class="agenciarubik-team-skill-wrap">
       <div class="agenciarubik-team-skill">
-        <h3>Segmentación Inteligente</h3>
+        <h3>Segmentación inteligente</h3>
         <p>Comenzamos por segmentar tu audiencia para personalizar los mensajes de manera que resuenen con las necesidades y preferencias específicas de cada grupo. Utilizamos datos demográficos, comportamiento de compra y niveles de interacción para asegurar que el mensaje correcto llegue a la persona correcta en el momento correcto.</p>
       </div>
       <div class="agenciarubik-team-skill">
-        <h3>Creación de Contenido Atractivo</h3>
+        <h3>Creación de contenido atractivo</h3>
         <p>Nuestros expertos en contenido y diseño gráfico crean campañas de correo electrónico visualmente atractivas y redactan copias persuasivas que capturan la esencia de tu marca. Desde newsletters informativos hasta promociones exclusivas, cada email está cuidadosamente diseñado para atraer, educar y convertir.</p>
       </div>
       <div class="agenciarubik-team-skill">
-        <h3>Automatización y Personalización</h3>
+        <h3>Automatización y personalización</h3>
         <p>Implementamos flujos de trabajo de automatización que envían correos electrónicos basados en acciones específicas de tus suscriptores, como la bienvenida a nuevos usuarios, recordatorios de carritos abandonados y seguimiento post-compra. Esto garantiza una comunicación oportuna y relevante que aumenta la lealtad y el valor de vida del cliente.</p>
       </div>
 
       <div class="agenciarubik-team-skill">
-        <h3>Pruebas A/B y Optimización de Base de Datos</h3>
+        <h3>Pruebas A/B y optimización de nase de datos</h3>
         <p>Realizamos pruebas A/B regularmente en elementos clave de tus campañas de email, como asuntos, imágenes y llamados a la acción, para identificar lo que mejor resuena con tu audiencia. Esta información optimiza futuras campañas, mejorando tasas de apertura, clics y conversiones. También mantenemos y actualizamos tus listas de contactos para asegurar la máxima efectividad.</p>
       </div>
 
       <div class="agenciarubik-team-skill">
-        <h3>Análisis y Reporte Profundo</h3>
+        <h3>Análisis y reporte profundo</h3>
         <p>Te proporcionamos análisis detallados y reportes comprensibles que muestran el rendimiento de tus campañas de email marketing. Desde la tasa de apertura hasta el retorno de inversión (ROI), monitoreamos meticulosamente los indicadores clave de rendimiento para ayudarte a tomar decisiones basadas en datos.</p>
       </div>
 
       <div class="agenciarubik-team-skill">
-        <h3>Cumplimiento y Mejores Prácticas</h3>
+        <h3>Cumplimiento y mejores prácticas</h3>
         <p>Nos aseguramos de que todas las campañas cumplan con las leyes de privacidad y regulaciones de email marketing, incluyendo GDPR y CAN-SPAM, protegiendo así tu reputación y asegurando la confianza de tus suscriptores.</p>
       </div>
 
@@ -74,7 +74,7 @@ include 'nav.php';
 
 <div class="container pt-5">
   <div class="agenciarubik-section-title center">
-    <h2>Transforma Tu Estrategia de Email Marketing Hoy</h2>
+    <h2>Transforma Tu estrategia de email marketing hoy</h2>
     <p class="mt-5">Con nuestro servicio de Email Marketing, tienes la garantía de no solo alcanzar a tu audiencia, sino de impactar de manera significativa, fomentando la conversión y fidelización. Ya sea que estés lanzando un nuevo producto, fomentando la lealtad del cliente o buscando aumentar tus ventas, estamos aquí para ayudarte a lograr tus objetivos de marketing con estrategias que entregan resultados.<br><br>Contacta con nosotros para comenzar a crear campañas que dejan huella y conducen al éxito de tu negocio.
     </p>
   </div>

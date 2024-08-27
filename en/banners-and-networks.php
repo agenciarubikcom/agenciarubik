28ae9907db3ad5f9fb954377281faf656a47e6dd
agenciarubikcom <gsuarez@agenciarubik.com>
2024-08-27 14:18:43 -0300

    Banners

---
+++ en/banners-and-networks.php
@@ -0,0 +1,157 @@
+<?php
+$URLTitleES = "es/banners-y-redes";
+$URLTitleEN = "en/banners-and-networks";
+$URLLink = "en/banners-and-networks";
+$Description = "Transform your digital presence with our banner design and social media management. Impact your audience with creative and effective solutions.";
+include '../header.php';
+include 'nav.php';
+?>
+
+<div class="agenciarubik-breadcrumb">
+  <div class="container">
+    <h1 class="post__title">Banners and Social Networks</h1>
+    <nav class="breadcrumbs">
+      <ul>
+        <li aria-current="page">Design</li>
+      </ul>
+    </nav>
+
+  </div>
+</div>
+<!-- End breadcrumb -->
+
+<div class="section mt-5">
+  <div class="container">
+    <div class="agenciarubik-section-title center">
+      <h2 class="rt-mb-20">Transform your digital presence with our design service!</h2>
+      <p>Banners, social networks and more: Everything you need to impact your audience.</p>
+    </div>
+  </div>
+</div>
+<!-- End section -->
+
+<div class="section mb-5">
+  <div class="container">
+    <div class="row agenciarubik-pricing-four-column" id="table-price-value">
+      <div class="col-xl-4 col-md-6">
+        <div class="agenciarubik-pricing-wrap">
+          <div class="agenciarubik-pricing-header">
+            <h3>Banner Pack</h3>
+          </div>
+          <div class="agenciarubik-pricing-description">
+            <p>Optimize the visual presence of your brand on the web with our Banners Package, designed to capture the attention of your potential clients and increase your online visibility.</p>
+          </div>
+          <div class="agenciarubik-pricing-body">
+            <ul>
+              <li><img src="../assets/images/v3/check.png" title="Check" alt="Check">5 banner sizes</li>
+              <li><img src="../assets/images/v3/check.png" title="Check" alt="Check">2 creatives per size</li>
+              <li><img src="../assets/images/v3/check.png" title="Check" alt="Check">2 HTML5 creatives</li>
+              <li><img src="../assets/images/v3/check.png" title="Check" alt="Check">1 reviews</li>
+              <li><br></li>
+              <li><br></li>
+              <li><br></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div class="col-xl-4 col-md-6">
+        <div class="agenciarubik-pricing-wrap">
+          <div class="agenciarubik-pricing-header">
+            <h3>RRSS Pack</h3>
+          </div>
+          <div class="agenciarubik-pricing-description">
+            <p>Boost your presence on social networks with our RRSS Package. Perfect for keeping your profiles active and connected with your audience in a constant and effective way.</p>
+          </div>
+          <div class="agenciarubik-pricing-body">
+            <ul>
+              <li><img src="../assets/images/v3/check.png" title="Check" alt="Check">15 images for posts</li>
+              <li><img src="../assets/images/v3/check.png" title="Check" alt="Check">5 reels for Instagram</li>
+              <li><img src="../assets/images/v3/check.png" title="Check" alt="Check">1 video</li>
+              <li><img src="../assets/images/v3/check.png" title="Check" alt="Check">Copy suggested</li>
+              <li><img src="../assets/images/v3/check.png" title="Check" alt="Check">2 reviews</li>
+              <li><br></li>
+              <li><br></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div class="col-xl-4 col-md-6">
+        <div class="agenciarubik-pricing-wrap">
+          <div class="agenciarubik-pricing-header">
+            <h3>Hybrid Pack</h3>
+          </div>
+          <div class="agenciarubik-pricing-description">
+            <p>Get the best of both worlds with our Hybrid Package. A complete solution that combines banners and content for social networks, ideal for a comprehensive and cohesive digital presence.</p>
+          </div>
+          <div class="agenciarubik-pricing-body">
+
+            <ul>
+              <li><img src="../assets/images/v3/check.png" title="Check" alt="Check">10 images for posts</li>
+              <li><img src="../assets/images/v3/check.png" title="Check" alt="Check">5 banner sizes</li>
+              <li><img src="../assets/images/v3/check.png" title="Check" alt="Check">1 HTML5 creative</li>
+              <li><img src="../assets/images/v3/check.png" title="Check" alt="Check">3 reels for Instagram</li>
+              <li><img src="../assets/images/v3/check.png" title="Check" alt="Check">1 video</li>
+              <li><img src="../assets/images/v3/check.png" title="Check" alt="Check">Copy suggested</li>
+              <li><img src="../assets/images/v3/check.png" title="Check" alt="Check">3 reviews</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+<div class="section">
+  <div class="container">
+    <div class="agenciarubik-section-title agenciarubik-two-column-title">
+      <div class="row">
+        <div class="col-lg-7">
+          <h2>Why choose our design packages?</h2>
+        </div>
+        <div class="col-lg-5 d-flex align-items-center">
+          <p>In a saturated digital world, standing out is essential. Our packages are designed to provide your brand with high-quality visual content that attracts, engages and converts. From animated banners to engaging social media content, we have everything you need to take your digital presence to the next level.</p>
+        </div>
+      </div>
+    </div>
+
+  </div>
+</div>
+
+<div class="section mt-5">
+  <div class="container">
+    <div class="row d-flex align-items-center">
+      <div class="col-lg-5">
+        <div class="agenciarubik-v3-thumb">
+          <img src="https://agenciarubik.com/assets/images/seo.svg" title="SEO" alt="SEO">
+        </div>
+      </div>
+      <div class="col-lg-7">
+        <div class="agenciarubik-section-title">
+          <h3>How does it work?</h3><br>
+          <h4>Choose your Package</h4>
+          <p>Select the package that best suits your needs.</p>
+          <h4>Customization</h4>
+          <p>Our team will work with you to personalize each item.</p>
+          <h4>Revisions</h4>
+          <p>We carry out the necessary revisions to ensure that you are 100% satisfied.</p>
+          <h4>Delivery</h4>
+          <p>Receive your banners and content ready to use.</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+<div class="section my-5">
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-12">
+        <?php include '../formcontact.php'; ?>
+      </div>
+    </div>
+  </div>
+</div>
+<!-- End section -->
+
+
+<?php include "../footer.php" ?>
---
+++ es/banners-y-redes.php
@@ -0,0 +1,155 @@
+<?php
+$URLTitleES = "es/banners-y-redes";
+$URLTitleEN = "en/banners-and-networks";
+$URLLink = "es/banners-y-redes";
+$Description = "Transforma tu presencia digital con nuestro diseño de banners y gestión de redes sociales. Impacta a tu audiencia con soluciones creativas y efectivas.";
+include '../header.php';
+include 'nav.php';
+?>
+
+<div class="agenciarubik-breadcrumb">
+    <div class="container">
+        <h1 class="post__title">Banners y Redes</h1>
+        <nav class="breadcrumbs">
+            <ul>
+                <li aria-current="page">Diseño</li>
+            </ul>
+        </nav>
+
+    </div>
+</div>
+<!-- End breadcrumb -->
+
+<div class="section mt-5">
+    <div class="container">
+        <div class="agenciarubik-section-title center">
+            <h2 class="rt-mb-20">¡Transforma tu presencia digital con nuestro servicio de diseño!</h2>
+            <p>Banners, redes sociales y más: Todo lo que necesitas para impactar a tu audiencia</p>
+        </div>
+    </div>
+</div>
+<!-- End section -->
+
+<div class="section mb-5">
+    <div class="container">
+      <div class="row agenciarubik-pricing-four-column" id="table-price-value">
+        <div class="col-xl-4 col-md-6">
+          <div class="agenciarubik-pricing-wrap">
+            <div class="agenciarubik-pricing-header">
+              <h3>Pack Banners</h3>
+            </div>
+
+            <div class="agenciarubik-pricing-description">
+              <p>Optimiza la presencia visual de tu marca en la web con nuestro Paquete Banners, diseñado para captar la atención de tus clientes potenciales y aumentar tu visibilidad online.</p>
+            </div>
+            <div class="agenciarubik-pricing-body">
+              <ul>
+                <li><img src="../assets/images/v3/check.png" title="Check" alt="Check">5 tamaños de banners</li>
+                <li><img src="../assets/images/v3/check.png" title="Check" alt="Check">2 creatividades por tamaño</li>
+                <li><img src="../assets/images/v3/check.png" title="Check" alt="Check">2 creatividades HTML5</li>
+                <li><img src="../assets/images/v3/check.png" title="Check" alt="Check">1 revisiones</li>
+                <li><br></li>
+                <li><br></li>
+                <li><br></li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div class="col-xl-4 col-md-6">
+          <div class="agenciarubik-pricing-wrap">
+            <div class="agenciarubik-pricing-header">
+              <h3>Pack RRSS</h3>
+            </div>
+            <div class="agenciarubik-pricing-description">
+              <p>Impulsa tu presencia en redes sociales con nuestro Paquete RRSS. Perfecto para mantener tus perfiles activos y conectados con tu audiencia de manera constante y efectiva.</p>
+            </div>
+            <div class="agenciarubik-pricing-body">
+
+              <ul>
+                <li><img src="../assets/images/v3/check.png" title="Check" alt="Check">15 imágenes para posteos</li>
+                <li><img src="../assets/images/v3/check.png" title="Check" alt="Check">5 reels para Instagram</li>
+                <li><img src="../assets/images/v3/check.png" title="Check" alt="Check">1 video</li>
+                <li><img src="../assets/images/v3/check.png" title="Check" alt="Check">Copy sugerido</li>
+                <li><img src="../assets/images/v3/check.png" title="Check" alt="Check">2 revisiones</li>
+                <li><br></li>
+                <li><br></li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div class="col-xl-4 col-md-6">
+          <div class="agenciarubik-pricing-wrap">
+            <div class="agenciarubik-pricing-header">
+              <h3>Pack Híbrido</h3>
+            </div>
+            <div class="agenciarubik-pricing-description">
+              <p>Consigue lo mejor de ambos mundos con nuestro Paquete Híbrido. Una solución completa que combina banners y contenido para redes sociales, ideal para una presencia digital integral y cohesiva.</p>
+            </div>
+            <div class="agenciarubik-pricing-body">
+
+              <ul>
+                <li><img src="../assets/images/v3/check.png" title="Check" alt="Check">10 imágenes para posteos</li>
+                <li><img src="../assets/images/v3/check.png" title="Check" alt="Check">5 tamaños de banners</li>
+                <li><img src="../assets/images/v3/check.png" title="Check" alt="Check">1 creatividad HTML5</li>
+                <li><img src="../assets/images/v3/check.png" title="Check" alt="Check">3 reels para Instagram</li>
+                <li><img src="../assets/images/v3/check.png" title="Check" alt="Check">1 video</li>
+                <li><img src="../assets/images/v3/check.png" title="Check" alt="Check">Copy sugerido</li>
+                <li><img src="../assets/images/v3/check.png" title="Check" alt="Check">3 revisiones</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="section">
+    <div class="container">
+      <div class="agenciarubik-section-title agenciarubik-two-column-title">
+        <div class="row">
+          <div class="col-lg-7">
+            <h2>¿Por qué elegir nuestros paquetes de diseño?</h2>
+          </div>
+          <div class="col-lg-5 d-flex align-items-center">
+            <p>En un mundo digital saturado, destacarse es esencial. Nuestros paquetes están diseñados para proporcionar a tu marca contenido visual de alta calidad que atrae, involucra y convierte. Desde banners animados hasta contenido atractivo para redes sociales, tenemos todo lo que necesitas para llevar tu presencia digital al siguiente nivel.</p>
+          </div>
+        </div>
+      </div>
+
+    </div>
+  </div>
+
+  <div class="section mt-5">
+    <div class="container">
+    <div class="row d-flex align-items-center">
+    <div class="col-lg-5">
+    <div class="agenciarubik-v3-thumb">
+        <img src="https://agenciarubik.com/assets/images/seo.svg" title="SEO" alt="SEO">
+    </div>
+    </div>
+    <div class="col-lg-7">
+        <div class="agenciarubik-section-title">
+            <h3>¿Cómo Funciona?</h3><br>
+            <h4>Elige tu Paquete</h4><p>Selecciona el paquete que mejor se adapte a tus necesidades.</p>
+            <h4>Personalización</h4><p>Nuestro equipo trabajará contigo para personalizar cada elemento.</p>
+            <h4>Revisiones</h4><p>Realizamos las revisiones necesarias para asegurarnos de que estés 100% satisfecho.</p>
+            <h4>Entrega</h4><p>Recibe tus banners y contenidos listos para usar.</p>
+        </div>
+        </div>
+        </div>
+    </div>
+</div>
+
+<div class="section my-5">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12">
+                <?php include '../formcontact.php'; ?>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- End section -->
+
+
+<?php include "../footer.php" ?>

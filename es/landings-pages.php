d80836feec966adc6aaa0354ecff13eff57e5062
agenciarubikcom <gsuarez@agenciarubik.com>
2025-02-22 20:58:56 -0300

    Landings Pages

--- es/landings-pages.php
+++ es/landings-pages.php
@@ -19,111 +19,128 @@ include 'nav.php';
     </div>
 </div>
 <!-- End breadcrumb -->
-
-<div class="section agenciarubik-section-padding2">
-<div class="container">
-  <div class="row align-items-center">
-
-    <div class="col-lg-6">
-    <h2 class="rt-mb-20">Impulsa tu negocio online</h2>
+<div class="section mt-5">
+    <div class="container">
+        <div class="agenciarubik-section-title ">
+            <h2 class="rt-mb-20">Impulsa tu negocio online</h2>
             <p>En la era digital, la primera interacción que un cliente potencial tiene con tu marca a menudo sucede online. Una landing page bien diseñada y optimizada es crucial para convertir ese primer clic en una conexión duradera. Nuestro servicio de desarrollo de landing pages está diseñado para capturar la esencia de tu negocio y comunicar eficazmente el valor de tu producto o servicio, garantizando una primera impresión memorable y convincente.</p>
+            <h3 class="mb-3">¿Qué es una Landing Page?</h3>
+            <p>Una landing page es una página web diseñada con un objetivo claro: convertir visitantes en leads o clientes. Sirve como el punto de entrada a una oferta específica, campaña o acción que deseas que tu audiencia tome. Al centrarse en un único objetivo, las landing pages son increíblemente efectivas para aumentar las tasas de conversión y maximizar el retorno de inversión en tus campañas de marketing digital.</p>
+
+        </div>
     </div>
-    <div class="col-lg-6">
-      <div class="agenciarubik-v3-thumb p-5">
-        <img src="https://agenciarubik.com/assets/images/landings-pages.svg" title="Landings Pages" alt="Landings Pages">
+</div>
+<!-- End section -->
+
+<div class="section mb-5">
+    <div class="container">
+    <div class="agenciarubik-accordion-wrap agenciarubik-accordion-wrap2" id="agenciarubik-accordion">
+        <div class="agenciarubik-accordion-item open">
+          <div class="agenciarubik-accordion-header">
+            <h3>Diseño personalizado y orientado a la conversión</h3>
+            <div class="agenciarubik-active-icon">
+              <img src="../assets/images/v3/plus.png" width="18px" alt="+">
+            </div>
+            <div class="agenciarubik-inactive-icon">
+              <img src="../assets/images/v3/minus.png" width="18px" alt="-">
+            </div>
+          </div>
+          <div class="agenciarubik-accordion-body">
+            <p>Desarrollamos cada landing page con un diseño personalizado que no solo refleja tu marca sino que también está optimizado para la conversión. Desde la elección de colores y tipografías hasta la disposición de los elementos en la página, cada detalle está pensado para guiar al visitante hacia la acción deseada.</p>
+          </div>
+        </div>
+        <div class="agenciarubik-accordion-item">
+          <div class="agenciarubik-accordion-header">
+            <h3>Hasta 5 módulos</h3>
+            <div class="agenciarubik-active-icon">
+              <img src="../assets/images/v3/plus.png" width="18px" alt="+">
+            </div>
+            <div class="agenciarubik-inactive-icon">
+              <img src="../assets/images/v3/minus.png" width="18px" alt="-">
+            </div>
+          </div>
+          <div class="agenciarubik-accordion-body">
+            <p>Nuestras landing pages pueden incluir hasta 5 módulos personalizables, permitiéndote contar la historia de tu servicio o producto de manera efectiva. Estos módulos pueden albergar textos persuasivos, vídeos explicativos, imágenes impactantes, galerías interactivas y formularios de contacto, entre otros, para ofrecer una experiencia rica y envolvente a tus visitantes.</p>
+          </div>
+        </div>
+        <div class="agenciarubik-accordion-item">
+          <div class="agenciarubik-accordion-header">
+            <h3>Cumplimiento de estándares de SEO, performance y accesibilidad</h3>
+            <div class="agenciarubik-active-icon">
+              <img src="../assets/images/v3/plus.png" width="18px" alt="+">
+            </div>
+            <div class="agenciarubik-inactive-icon">
+              <img src="../assets/images/v3/minus.png" width="18px" alt="-">
+            </div>
+          </div>
+          <div class="agenciarubik-accordion-body">
+            <p>Entendemos la importancia de la visibilidad online, la velocidad de carga y la accesibilidad. Por ello, aseguramos que todas nuestras landing pages cumplen con los estándares más actuales de SEO para una mejor posición en los motores de búsqueda, optimizamos la performance para una carga rápida y eficiente, y seguimos las directrices de accesibilidad web para garantizar que tu mensaje llegue a todos los usuarios.</p>
+          </div>
+        </div>
+        <div class="agenciarubik-accordion-item">
+          <div class="agenciarubik-accordion-header">
+            <h3>Soporte continuo y análisis de rendimiento</h3>
+            <div class="agenciarubik-active-icon">
+              <img src="../assets/images/v3/plus.png" width="18px" alt="+">
+            </div>
+            <div class="agenciarubik-inactive-icon">
+              <img src="../assets/images/v3/minus.png" width="18px" alt="-">
+            </div>
+          </div>
+          <div class="agenciarubik-accordion-body">
+            <p>Nuestro compromiso con tu éxito no termina con el lanzamiento de la página. Ofrecemos soporte continuo y análisis de rendimiento para asegurar que tu landing page siga siendo efectiva a lo largo del tiempo, adaptándonos a cambios en el mercado y en las preferencias de tu audiencia.</p>
+          </div>
+        </div>
+        <div class="agenciarubik-accordion-item">
+          <div class="agenciarubik-accordion-header">
+            <h3>Contenido optimizado y valioso</h3>
+            <div class="agenciarubik-active-icon plus">
+              <img src="../assets/images/v3/plus.png" width="18px" alt="+">
+            </div>
+            <div class="agenciarubik-inactive-icon">
+              <img src="../assets/images/v3/minus.png" width="18px" alt="-">
+            </div>
+          </div>
+          <div class="agenciarubik-accordion-body">
+            <p>Creación de contenido fresco, relevante y optimizado para SEO que resuene con tu audiencia y posicione tu sitio como una autoridad en tu nicho. Desde blogs hasta guías y estudios de caso, el contenido de calidad es fundamental para atraer y retener a tu audiencia.</p>
+          </div>
+        </div>
+        <div class="agenciarubik-accordion-item">
+          <div class="agenciarubik-accordion-header">
+            <h3>Mejora de la velocidad y experiencia del usuario</h3>
+            <div class="agenciarubik-active-icon plus">
+              <img src="../assets/images/v3/plus.png" width="18px" alt="+">
+            </div>
+            <div class="agenciarubik-inactive-icon">
+              <img src="../assets/images/v3/minus.png" width="18px" alt="-">
+            </div>
+          </div>
+          <div class="agenciarubik-accordion-body">
+            <p>La velocidad de carga del sitio web y la experiencia del usuario son factores críticos para el SEO. Trabajamos para optimizar estos aspectos, asegurando que tu sitio web sea rápido, responsive y fácil de navegar en cualquier dispositivo.</p>
+          </div>
+        </div>
       </div>
     </div>
-
   </div>
-</div>
-</div>
 
 
-<div class="section mt-5">
+  <div class="section my-2">
     <div class="container">
+    <div class="row d-flex align-items-center">
+    <div class="col-lg-5">
+    <div class="agenciarubik-v3-thumb">
+        <img width="500px" src="/assets/images/landings-pages.svg" title="Landings Pages" alt="Landings Pages">
+    </div>
+    </div>
+    <div class="col-lg-7">
         <div class="agenciarubik-section-title">
-            <h3 class="rt-mb-20">¿Qué es una Landing Page?</h3>
-            <p>Una landing page es una página web diseñada con un objetivo claro: convertir visitantes en leads o clientes. Sirve como el punto de entrada a una oferta específica, campaña o acción que deseas que tu audiencia tome. Al centrarse en un único objetivo, las landing pages son increíblemente efectivas para aumentar las tasas de conversión y maximizar el retorno de inversión en tus campañas de marketing digital.</p>
+        <h2 class="mb-5">Transforma visitantes en clientes con nuestras Landing Pages</h2>
+        <p>Con una combinación de diseño atractivo, contenido persuasivo y tecnología avanzada, nuestras landing pages son la herramienta perfecta para captar la atención de tu audiencia y convertir el interés en acción. Ya sea que busques generar leads, aumentar las ventas o promover un evento, nuestras soluciones personalizadas te ayudarán a alcanzar tus objetivos</p>
+        </div>
+        </div>
         </div>
     </div>
 </div>
-<!-- End section -->
-
-<div class="section agenciarubik-section-padding5">
-    <div class="container">
-        <div class="row">
-            <div class="col-lg-7">
-                <div class="agenciarubik-default-content m-right-50 sticky">
-                    <h2>Nuestro enfoque en el desarrollo de Landing Pages</h2>
-                </div>
-            </div>
-            <div class="col-lg-5">
-                <div class="agenciarubik-default-content">
-                    <div class="agenciarubik-iconbox-wrap-left mw-100">
-
-                        <div class="agenciarubik-iconbox-data data-small">
-                            <span>Diseño personalizado y orientado a la conversión</span>
-                            <p>Desarrollamos cada landing page con un diseño personalizado que no solo refleja tu marca sino que también está optimizado para la conversión. Desde la elección de colores y tipografías hasta la disposición de los elementos en la página, cada detalle está pensado para guiar al visitante hacia la acción deseada.</p>
-                        </div>
-                    </div>
-
-                    <div class="agenciarubik-iconbox-wrap-left mw-100">
-
-                        <div class="agenciarubik-iconbox-data data-small">
-                            <span>Hasta 5 Módulos</span>
-                            <p>Nuestras landing pages pueden incluir hasta 5 módulos personalizables, permitiéndote contar la historia de tu servicio o producto de manera efectiva. Estos módulos pueden albergar textos persuasivos, vídeos explicativos, imágenes impactantes, galerías interactivas y formularios de contacto, entre otros, para ofrecer una experiencia rica y envolvente a tus visitantes.</p>
-                        </div>
-                    </div>
-
-                    <div class="agenciarubik-iconbox-wrap-left mw-100">
-
-                        <div class="agenciarubik-iconbox-data data-small">
-                            <span>Cumplimiento de estándares de SEO, performance y accesibilidad</span>
-                            <p>Entendemos la importancia de la visibilidad online, la velocidad de carga y la accesibilidad. Por ello, aseguramos que todas nuestras landing pages cumplen con los estándares más actuales de SEO para una mejor posición en los motores de búsqueda, optimizamos la performance para una carga rápida y eficiente, y seguimos las directrices de accesibilidad web para garantizar que tu mensaje llegue a todos los usuarios.</p>
-                        </div>
-                    </div>
-
-                    <div class="agenciarubik-iconbox-wrap-left mw-100">
-
-                        <div class="agenciarubik-iconbox-data data-small">
-                            <span>Soporte continuo y análisis de rendimiento</span>
-                            <p>Nuestro compromiso con tu éxito no termina con el lanzamiento de la página. Ofrecemos soporte continuo y análisis de rendimiento para asegurar que tu landing page siga siendo efectiva a lo largo del tiempo, adaptándonos a cambios en el mercado y en las preferencias de tu audiencia.</p>
-                        </div>
-                    </div>
-
-
-                    <div class="agenciarubik-iconbox-wrap-left mw-100">
-
-                        <div class="agenciarubik-iconbox-data data-small">
-                            <span>Contenido optimizado y valioso</span>
-                            <p>Creación de contenido fresco, relevante y optimizado para SEO que resuene con tu audiencia y posicione tu sitio como una autoridad en tu nicho. Desde blogs hasta guías y estudios de caso, el contenido de calidad es fundamental para atraer y retener a tu audiencia.</p>
-                        </div>
-                    </div>
-
-
-                    <div class="agenciarubik-iconbox-wrap-left mw-100">
-
-                        <div class="agenciarubik-iconbox-data data-small">
-                            <span>Mejora de la velocidad y experiencia del usuario</span>
-                            <p>La velocidad de carga del sitio web y la experiencia del usuario son factores críticos para el SEO. Trabajamos para optimizar estos aspectos, asegurando que tu sitio web sea rápido, responsive y fácil de navegar en cualquier dispositivo.</p>
-                        </div>
-                    </div>
-
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-
-<div class="section mt-5">
-    <div class="container">
-        <div class="agenciarubik-section-title">
-            <h3 class="rt-mb-20">Transforma visitantes en clientes con nuestras Landing Pages</h3>
-            <p>Con una combinación de diseño atractivo, contenido persuasivo y tecnología avanzada, nuestras landing pages son la herramienta perfecta para captar la atención de tu audiencia y convertir el interés en acción. Ya sea que busques generar leads, aumentar las ventas o promover un evento, nuestras soluciones personalizadas te ayudarán a alcanzar tus objetivos.</p>
-        </div>
-    </div>
-</div>
-<!-- End section -->
 
 <div class="section my-5">
     <div class="container">

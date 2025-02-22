4da75ab9b8f09d034178faa7df429c19ca48f079
agenciarubikcom <gsuarez@agenciarubik.com>
2025-02-22 16:53:29 -0300

    Ajustes y Paginas nuevas

--- es/dominios.php
+++ es/dominios.php
@@ -20,109 +20,130 @@ include 'nav.php';
 </div>
 <!-- End breadcrumb -->
 
-
-
-<div class="section agenciarubik-section mt-5">
-
-    <div class="container mb-5 ">
-        <div class="row d-flex align-items-center">
-            <div class="col-lg-5 p-5">
-                <div class="agenciarubik-v3-thumb">
-                    <img src="https://agenciarubik.com/assets/images/dominios.svg" title="Dominios" alt="Dominios">
-
-                </div>
-            </div>
-            <div class="col-lg-7">
-                <div class="agenciarubik-default-content">
-                    <h2 class="rt-mb-20">Encuentra el nombre perfecto</h2>
-                    <p>Entendemos que un buen dominio es esencial para el éxito de tu presencia en internet. Ofrecemos un servicio completo de venta de dominios, diseñado para ayudarte a encontrar y registrar el nombre perfecto para tu negocio, proyecto o marca personal.</p>
-                </div>
-            </div>
+<div class="section mt-5">
+    <div class="container">
+        <div class="mb-5 ">
+            <h2 class="rt-mb-20">Encuentra el nombre perfecto</h2>
+            <p>Entendemos que un buen dominio es esencial para el éxito de tu presencia en internet. Ofrecemos un servicio completo de venta de dominios, diseñado para ayudarte a encontrar y registrar el nombre perfecto para tu negocio, proyecto o marca personal.</p>
+            <h3>Nuestros servicios incluyen:</h3>
         </div>
     </div>
 </div>
 <!-- End section -->
 
-<div class="section agenciarubik-section">
+<div class="section mb-5">
     <div class="container">
-        <div class="col-lg-12">
-            <div class="agenciarubik-default-content">
-                <h2 class="mb-5">Nuestros servicios incluyen</h2>
+    <div class="agenciarubik-accordion-wrap agenciarubik-accordion-wrap2" id="agenciarubik-accordion">
+        <div class="agenciarubik-accordion-item open">
+          <div class="agenciarubik-accordion-header">
+            <h4>Registro de Dominios</h4>
+            <div class="agenciarubik-active-icon">
+              <img src="../assets/images/v3/plus.png" width="18px" alt="+">
             </div>
-        </div>
-        <div class="agenciarubik-team-skill-wrap">
-            <div class="agenciarubik-team-skill">
-                <h3>Registro de Dominios</h3>
-                <p>Te ayudamos a registrar tu dominio en las extensiones más populares (.com, .net, .org, entre otras). Nos aseguramos de que el dominio quede registrado a nombre del cliente o, si lo prefieres, a nombre de nuestra agencia para facilitar la gestión.</p>
+            <div class="agenciarubik-inactive-icon">
+              <img src="../assets/images/v3/minus.png" width="18px" alt="-">
             </div>
-            <div class="agenciarubik-team-skill">
-                <h3>Trabajo de Naming</h3>
-                <p>Sabemos que elegir el nombre adecuado puede ser un desafío. Nuestro equipo de expertos realiza un trabajo de naming exhaustivo para ayudarte a encontrar un dominio que sea memorable, relevante y alineado con tu marca.</p>
-            </div>
-            <div class="agenciarubik-team-skill">
-                <h3>Verificación de Disponibilidad</h3>
-                <p>Verificamos la disponibilidad de los dominios en tiempo real y te informamos sobre las opciones disponibles. Ten en cuenta que los costos y la disponibilidad pueden variar según la demanda y la extensión del dominio.</p>
-            </div>
-
-            <div class="agenciarubik-team-skill">
-                <h3>Gestión de Renovaciones</h3>
-                <p>Nos encargamos de la renovación de tu dominio para que no tengas que preocuparte por perder tu nombre en línea. Te notificamos con antelación sobre las fechas de renovación y gestionamos todo el proceso por ti.</p>
-            </div>
-
-            <div class="agenciarubik-team-skill">
-                <h3>Transferencias de Dominio</h3>
-                <p>Facilitamos la transferencia de dominios entre registradores, asegurando un proceso rápido y sin complicaciones.</p>
-            </div>
-
-            <div class="agenciarubik-team-skill">
-                <h3>Protección de Privacidad</h3>
-                <p>Ofrecemos servicios de protección de privacidad para que tus datos personales no sean visibles en los registros públicos de dominios, garantizando así tu seguridad y tranquilidad.</p>
-            </div>
-
-        </div>
-    </div>
-</div>
-
-<div class="container mb-5">
-    <div class="row d-flex align-items-center">
-
-        <div class="col-lg-12">
-
-            <div class="agenciarubik-default-content mt-5">
-                <h2 class="mb-3">Por qué elegirnos</h2>
-            </div>
-
-            <div class="agenciarubik-default-content left">
-
-                <div>
-                <h3>Asesoramiento Personalizado</h3><p>Trabajamos contigo para entender tus necesidades y objetivos, proporcionando recomendaciones personalizadas para elegir el mejor dominio para tu proyecto.</p>
-
-                <h3>Transparencia y Flexibilidad</h3><p>Ofrecemos opciones claras para el registro de dominios, ya sea a tu nombre o al de nuestra agencia, según lo que más te convenga.</p>
-
-                <h3>Experiencia y Profesionalismo</h3><p>Con años de experiencia en el sector, nuestro equipo está comprometido en ofrecer un servicio de alta calidad y soporte continuo.</p>
-
-
-                </div>
-            </div>
-        </div>
-
-    </div>
-</div>
-
-<div class="section agenciarubik-section-padding mt-5" style="background-color:#ABFB4F;">
-    <div class="container">
-      <div class="row">
-        <div class="col-lg-6">
-          <div class="agenciarubik-default-content cta dark">
-            <h2>Comienza hoy mismo</h2>
+          </div>
+          <div class="agenciarubik-accordion-body">
+            <p>Te ayudamos a registrar tu dominio en las extensiones más populares (.com, .net, .org, entre otras). Nos aseguramos de que el dominio quede registrado a nombre del cliente o, si lo prefieres, a nombre de nuestra agencia para facilitar la gestión.</p>
           </div>
         </div>
-        <div class="col-lg-6 d-flex align-items-center dark">
-          <p>No dejes que la elección de un dominio adecuado sea un obstáculo para tu éxito en línea. Contáctanos hoy para una consulta gratuita y descubre cómo podemos ayudarte a encontrar y registrar el nombre perfecto para tu presencia en internet.</p>
+        <div class="agenciarubik-accordion-item">
+          <div class="agenciarubik-accordion-header">
+            <h4>Trabajo de Naming</h4>
+            <div class="agenciarubik-active-icon">
+              <img src="../assets/images/v3/plus.png" width="18px" alt="+">
+            </div>
+            <div class="agenciarubik-inactive-icon">
+              <img src="../assets/images/v3/minus.png" width="18px" alt="-">
+            </div>
+          </div>
+          <div class="agenciarubik-accordion-body">
+            <p>Sabemos que elegir el nombre adecuado puede ser un desafío. Nuestro equipo de expertos realiza un trabajo de naming exhaustivo para ayudarte a encontrar un dominio que sea memorable, relevante y alineado con tu marca.</p>
+          </div>
+        </div>
+        <div class="agenciarubik-accordion-item">
+          <div class="agenciarubik-accordion-header">
+            <h4>Verificación de Disponibilidad</h4>
+            <div class="agenciarubik-active-icon">
+              <img src="../assets/images/v3/plus.png" width="18px" alt="+">
+            </div>
+            <div class="agenciarubik-inactive-icon">
+              <img src="../assets/images/v3/minus.png" width="18px" alt="-">
+            </div>
+          </div>
+          <div class="agenciarubik-accordion-body">
+            <p>Verificamos la disponibilidad de los dominios en tiempo real y te informamos sobre las opciones disponibles. Ten en cuenta que los costos y la disponibilidad pueden variar según la demanda y la extensión del dominio.</p>
+          </div>
+        </div>
+        <div class="agenciarubik-accordion-item">
+          <div class="agenciarubik-accordion-header">
+            <h4>Gestión de Renovaciones</h4>
+            <div class="agenciarubik-active-icon">
+              <img src="../assets/images/v3/plus.png" width="18px" alt="+">
+            </div>
+            <div class="agenciarubik-inactive-icon">
+              <img src="../assets/images/v3/minus.png" width="18px" alt="-">
+            </div>
+          </div>
+          <div class="agenciarubik-accordion-body">
+            <p>Nos encargamos de la renovación de tu dominio para que no tengas que preocuparte por perder tu nombre en línea. Te notificamos con antelación sobre las fechas de renovación y gestionamos todo el proceso por ti.</p>
+          </div>
+        </div>
+        <div class="agenciarubik-accordion-item">
+          <div class="agenciarubik-accordion-header">
+            <h4>Transferencias de Dominio</h4>
+            <div class="agenciarubik-active-icon plus">
+              <img src="../assets/images/v3/plus.png" width="18px" alt="+">
+            </div>
+            <div class="agenciarubik-inactive-icon">
+              <img src="../assets/images/v3/minus.png" width="18px" alt="-">
+            </div>
+          </div>
+          <div class="agenciarubik-accordion-body">
+            <p>Facilitamos la transferencia de dominios entre registradores, asegurando un proceso rápido y sin complicaciones.</p>
+          </div>
+        </div>
+        <div class="agenciarubik-accordion-item">
+          <div class="agenciarubik-accordion-header">
+            <h4>Protección de Privacidad</h4>
+            <div class="agenciarubik-active-icon plus">
+              <img src="../assets/images/v3/plus.png" width="18px" alt="+">
+            </div>
+            <div class="agenciarubik-inactive-icon">
+              <img src="../assets/images/v3/minus.png" width="18px" alt="-">
+            </div>
+          </div>
+          <div class="agenciarubik-accordion-body">
+            <p>Ofrecemos servicios de protección de privacidad para que tus datos personales no sean visibles en los registros públicos de dominios, garantizando así tu seguridad y tranquilidad.</p>
+          </div>
         </div>
       </div>
     </div>
   </div>
+
+
+  <div class="section my-2">
+    <div class="container">
+    <div class="row d-flex align-items-center">
+    <div class="col-lg-5">
+    <div class="agenciarubik-v3-thumb">
+        <img width="500px" src="/assets/images/dominios.svg" title="Dominios" alt="Dominios">
+    </div>
+    </div>
+    <div class="col-lg-7">
+    <div class="agenciarubik-section-title">
+            <h3 class="mb-3">¿Por qué elegirnos?</h3>
+            <h4>Asesoramiento Personalizado:</h4><p>Trabajamos contigo para entender tus necesidades y objetivos, proporcionando recomendaciones personalizadas para elegir el mejor dominio para tu proyecto.</p>
+            <h4>Transparencia y Flexibilidad:</h4><p>Ofrecemos opciones claras para el registro de dominios, ya sea a tu nombre o al de nuestra agencia, según lo que más te convenga.</p>
+            <h4>Experiencia y Profesionalismo:</h4><p>Con años de experiencia en el sector, nuestro equipo está comprometido en ofrecer un servicio de alta calidad y soporte continuo.</p>
+            <h3 class="mt-5 mb-3">Comienza hoy mismo</h3><p>No dejes que la elección de un dominio adecuado sea un obstáculo para tu éxito en línea. Contáctanos hoy para una consulta gratuita y descubre cómo podemos ayudarte a encontrar y registrar el nombre perfecto para tu presencia en internet.</p>
+        </div>
+        </div>
+        </div>
+    </div>
+</div>
+
 
 <div class="section my-5">
     <div class="container">
--- es/kit-digital.php
+++ es/kit-digital.php
@@ -19,63 +19,71 @@ include 'nav.php';
     </div>
 </div>
 
-<div class="section agenciarubik-section-padding2">
+<div class="section mt-5">
     <div class="container">
-        <div class="row align-items-center">
-            <div class="col-lg-6">
-                <div class="agenciarubik-default-content">
-                    <h2 class="rt-mb-20">Tu puerta de entrada al mundo online</h2>
-                </div>
-            </div>
-            <div class="col-lg-6">
-                <div class="agenciarubik-v3-thumb py-2">
-                    <p>¿Eres una persona emprendedora o una pequeña empresa sin presencia en internet? ¿Estás buscando una manera fácil y efectiva de llevar tu negocio al siguiente nivel? ¡Tenemos la solución perfecta para ti!</p>
-                </div>
-            </div>
 
-        </div>
-    </div>
-</div>
+        <div class="agenciarubik-section-title">
 
-<div class="container pt-5">
-    <div class="agenciarubik-section-title">
-        <h3>¿Qué es el Kit Digital?</h3>
-        <p class="mt-5">Un paquete integral diseñado especialmente para aquellos que desean dar el salto al mundo digital sin complicaciones. Nuestro <b>Kit Digital</b> incluye todo lo que necesitas para establecer una presencia en línea profesional y atractiva, ¡y mucho más!<br><br>
+            <h2 class="rt-mb-20">Tu puerta de entrada al mundo online</h2>
+            <p>¿Eres una persona emprendedora o una pequeña empresa sin presencia en internet? ¿Estás buscando una manera fácil y efectiva de llevar tu negocio al siguiente nivel? ¡Tenemos la solución perfecta para ti!</p>
 
-            El Kit Digital incluye un <b>sitio web personalizado</b> con un diseño profesional y moderno que reflejará la esencia de tu negocio. Además, optimizamos tu sitio web con técnicas SEO para mejorar tu visibilidad en Google y otros buscadores. Aseguramos que tu sitio sea responsivo, lo que significa que se verá increíble en cualquier dispositivo, ya sea un móvil, tablet o computadora. También incluimos un formulario de contacto para facilitar la comunicación con tus clientes potenciales.<br><br>
-
+            <h3 class="mb-2">¿Qué es el Kit Digital?</h3>
+            <p>Nuestro servicio de SEO para páginas web está diseñado para catapultar tu sitio a las primeras posiciones en los resultados de búsqueda, aumentando tu visibilidad y atrayendo más tráfico cualificado. Descubre cómo podemos transformar la presencia en línea de tu negocio.
+            Un paquete integral diseñado especialmente para aquellos que desean dar el salto al mundo digital sin complicaciones. Nuestro <b>Kit Digital</b> incluye todo lo que necesitas para establecer una presencia en línea profesional y atractiva, ¡y mucho más!</p>
+            <div class="row d-flex">
+        <div class="col-lg-7">
+            <p>
+            El Kit Digital incluye un <b>sitio web personalizado</b> con un diseño profesional y moderno que reflejará la esencia de tu negocio. Además, optimizamos tu sitio web con técnicas SEO para mejorar tu visibilidad en Google y otros buscadores. Aseguramos que tu sitio sea responsivo, lo que significa que se verá increíble en cualquier dispositivo, ya sea un móvil, tablet o computadora. También incluimos un formulario de contacto para facilitar la comunicación con tus clientes potenciales. </p>
+            <p>
             El servicio de <b>alojamiento web</b> que ofrecemos es de alta calidad, garantizando que tu sitio web esté siempre en línea de manera segura y confiable. Incluimos un certificado SSL para proteger la información de tus visitantes y ganar su confianza.<br><br>
 
-            Te asistimos en la elección y registro de un <b>dominio personalizado</b>, asegurándonos de que el nombre de dominio sea perfecto para tu negocio.<br><br>
+
+            </p>
+       </div>
+            <div class="col-lg-5">
+            <div class="agenciarubik-v3-thumb">
+        <img width="500px" src="/assets/images/kit-digital-2.svg" title="KIT Digital" alt="KIT Digital">
+            </div>
+            </div>
+            <div class="col-lg-12">
+                <p>
+                Te asistimos en la elección y registro de un <b>dominio personalizado</b>, asegurándonos de que el nombre de dominio sea perfecto para tu negocio.<br><br>
+
 
             Además, proporcionamos un <b>pack de imágenes profesionales</b> diseñadas específicamente para tus redes sociales, facilitando la creación de publicaciones atractivas para Facebook, Instagram, X y LinkedIn. También incluimos plantillas personalizables para que puedas mantener una estética coherente en tus publicaciones.<br><br>
-
             Nuestro Kit Digital también incluye mantenimiento y soporte técnico 24/7 vía mail para resolver cualquier problema y realizar actualizaciones de seguridad constantes. Configuramos Google Analytics para que obtengas datos valiosos sobre el tráfico y rendimiento de tu sitio. También incluimos <b>SEO local</b> para ayudarte a atraer a clientes en tu área, mejorando tu presencia en búsquedas locales y mapas.
         </p>
-    </div>
-
-</div>
-
-<div class="section">
-    <div class="container">
-        <div class="agenciarubik-section-title agenciarubik-two-column-title">
-            <div class="row align-items-center">
-                <div class="col-lg-6">
-                    <div class="agenciarubik-v3-thumb p-5">
-                        <img src="https://agenciarubik.com/assets/images/kit-digital.svg" title="Kit Digital" alt="Kit Digital">
-                    </div>
-                </div>
-                <div class="col-lg-6">
-                    <h2>¡Empieza Hoy!</h2>
-                    <p>Establecer tu presencia en línea nunca ha sido tan fácil. Contáctanos y descubre cómo el Kit Digital puede ayudarte a crecer y destacar en el mundo digital. ¡Da el primer paso hacia el éxito con el Kit Digital!</p>
-                </div>
-            </div>
         </div>
 
+
+</div>
+        </div>
+        </div>
+    </div>
+</div>
+<!-- End section -->
+
+<div class="section mb-5">
+
+  <div class="section my-2">
+    <div class="container">
+    <div class="row d-flex align-items-center">
+    <div class="col-lg-5">
+    <div class="agenciarubik-v3-thumb">
+        <img width="500px" src="/assets/images/kit-digital.svg" title="KIT Digital" alt="KIT Digital">
+    </div>
+    </div>
+    <div class="col-lg-7">
+        <div class="agenciarubik-section-title">
+        <h2 class="mb-3">¡Empieza Hoy!</h2>
+        <p>Establecer tu presencia en línea nunca ha sido tan fácil. Contáctanos y descubre cómo el Kit Digital puede ayudarte a crecer y destacar en el mundo digital.<br><br><b>¡Da el primer paso hacia el éxito con el Kit Digital!</b></p>
+        </div>
+        </div>
+        </div>
     </div>
 </div>
 
-</div>
+
 
 <div class="section my-5">
     <div class="container">
